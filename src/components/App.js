a74810619a9c4de6100f48a5766b41dede85761e
josh356 <joshua.canuel@gmail.com>
2021-10-05 20:48:49 -0400

    removed trailing /

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ function App() {
   //console.log(JSON.parse(localStorage.getItem("cartItemsKey")))
   //console.log(itemListings)
   return (
-    <Router basename = "/my-app/">
+    <Router basename = "/my-app">
       <Navbar />
       <button onClick = {() => 
         {
